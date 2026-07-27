0913aceeee40dcccc1973150826d7c1d82d55003
mayankbhatwadekar88 <mayankbhatwadekar98@gmail.com>
2026-07-27 07:09:40 +0000

    Added remote exec

--- provisioners/main.tf
+++ provisioners/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 6.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
@@ -12,10 +12,21 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2_demo" {
-  ami = "ami-078fe7ff43e10cf8c"
-  instance_type = "t3.micro"
+  ami                    = "ami-078fe7ff43e10cf8c"
+  instance_type          = "t3.micro"
+  key_name               = "linux_ohio"
+  vpc_security_group_ids = ["sg-04fa4e3a4b2217e52"]
 
-  provisioner "local-exec" {
-    command = "echo ${self.private_ip} >> private_ip.txt" 
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("./linux_ohio.pem")
+    host        = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo systemctl start nginx"
+    ]
   }
 }
